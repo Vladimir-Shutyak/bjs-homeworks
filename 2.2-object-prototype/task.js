b447f67f20c7565d923620392edb426eae729a61
Vladimir-Shutyak <vladimir.shutyak@mail.ru>
2021-04-22 19:34:00 +0300

    Lesson 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,8 +1,40 @@
 //String.prototype.isPalindrome - для задачи №1
 
+function isPalindrome(){
+    let strReverse = this.split('').reverse().join('').replace(/\s+/g, '').toLowerCase(''); 
+    
+    if (strReverse === this.replace(/\s+/g, '').toLowerCase('')) {
+        return (true);
+    } 
+    else {
+        return (false);
+    }
+}
+
+String.prototype.isPalindrome = isPalindrome;
+
+
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark
+    let average;
+    let roundedAverage;
+    let markslen = marks.length;
+    average = 0;
+    roundedAverage = 0;
+    
+    if (markslen === 0){
+        return (roundedAverage);
+    }
+    else {
+        for (let i=0; i<markslen; i++){
+            average = average + marks[i];
+        }
+
+        average = average / markslen;
+        roundedAverage = Math.round(average);
+        
+        return (roundedAverage);
+    }
 }
 
 function checkBirthday(birthday) {
