917dae178824a71be5c0bae4cc9b0c6b0aa02b40
Vladimir-Shutyak <vladimir.shutyak@mail.ru>
2021-04-24 18:31:26 +0300

    Re-working

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,40 +1,43 @@
-function calculateTotalMortgage(percent, contribution, amount, date) {
+function calculateTotalMortgage(percent, contribution, amount, dateEnd) {
     // код для задачи №1 писать здесь
-    "use strict";
-  
+    "use strict"; 
     let today = new Date();
-    let dateEnd = new Date(date);
-    let totalAmount = 0;
-    let S = 0;
-    let P = 0;
-    let n = 0;
 
-    if (isNaN (parseFloat (percent))) {
+    if ( (isNaN (parseFloat (percent))) || (percent <0) ) {
         alert(`Параметр percent содержит неправильное значение ${parseFloat (percent)}`);
     }
 
-    if (isNaN (parseFloat (contribution))) {
+    if (isNaN (parseFloat (contribution)) || (contribution <0) ) {
         alert(`Параметр contribution содержит неправильное значение ${parseFloat (contribution)}`);
     }
 
-    if (isNaN (parseFloat (amount))) {
+    if (isNaN (parseFloat (amount)) || (amount <0) ) {
         alert(`Параметр amount содержит неправильное значение ${parseFloat (amount)}`);
     }
     
-    S = amount - contribution;
-    n = ( Math.floor ( ( ( dateEnd - today ) / ( 1000 * 3600 * 24 ) ) / 30 ) );
-    P =  ( percent / 100 ) / 12 ;
-    totalAmount =  S * ( P + P / ( ( ( 1 + P ) ** n ) - 1 ) );
+    
+
+    if ( (dateEnd < today) || (dateEnd.length === undefined) ) {
+        alert(`Параметр date содержит пустую дату или предыдущую ${dateEnd}`);
+    }
+
+    let S = amount - contribution;
+    let P = ( percent / 100 ) / 12 ;
+    let n = (dateEnd.getFullYear() - today.getFullYear() ) * 12 +  (dateEnd.getMonth() - today.getMonth() );   
+    let totalAmount =  (S * ( P + P / ( ( ( 1 + P ) ** n ) - 1 ) ) * n);
+    
     totalAmount = totalAmount.toFixed(2);
     
-    return totalAmount
+    return (parseFloat(totalAmount));
 }
-
 
 function getGreeting(name) {
 
     // код для задачи №2 писать здесь
-    
+    if (name === undefined){
+        return `Привет, мир! Меня зовут Аноним.`;   
+    }
+
     if (name.length > 0) {
         return `Привет, мир! Меня зовут ${name}.`; 
     }
